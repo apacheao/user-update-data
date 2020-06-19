2dafdae5389cbb924ed9d1e4729414a75306458f
Tim-ao <1194522057@qq.com>
2020-06-19 22:36:01 +0800

    Create c.vue

---
+++ demo/c.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <textarea v-model="detail"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                detail: '',
+                initUpdateNum: 0,
+            }
+        },
+        updated() {
+            this.initUpdateNum++
+            this.$emit('cComponentChangeEvent', this.initUpdateNum)
+            console.log(`c更新次数`, this.initUpdateNum)
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
